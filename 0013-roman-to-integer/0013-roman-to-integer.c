16f9cfb3e4401af3315bc57f9443e39a6e7214b7
Satyam shaw <mayankrajkkr@gmail.com>
2026-07-01 00:58:40 +0530

    Time: 0 ms (100%), Space: 10.9 MB (95.47%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,45 +1,33 @@
-int romanToInt(char* s) {
-    int len = strlen(s);
-    int i,result = 0;
-
-    for(i = 0;i < len;i++)
+int value(char s)
+{
+    switch(s)
     {
-        int value = 0;
-        switch(s[i])
-        {
-            case 'I': value = 1; break;
-            case 'V': value = 5; break;
-            case 'X': value = 10; break;
-            case 'L': value = 50; break;
-            case 'C': value = 100; break;
-            case 'D': value = 500; break;
-            case 'M': value = 1000; break;
-            default: value = 0; break;
-        }
-
-        if(i+1 < len)
-        {
-            int nextvalue = 0;
-            switch(s[i+1])
-        {
-            case 'I': nextvalue = 1; break;
-            case 'V': nextvalue = 5; break;
-            case 'X': nextvalue = 10; break;
-            case 'L': nextvalue = 50; break;
-            case 'C': nextvalue = 100; break;
-            case 'D': nextvalue = 500; break;
-            case 'M': nextvalue = 1000; break;
-            default: nextvalue = 0; break;
-        }
-
-        if(value < nextvalue)
-        {
-            result = result - value;
-            continue;
-        }
-
-        }
-        result = result + value;
+        case 'I': return 1; break;
+        case 'V': return 5; break;
+        case 'X': return 10; break;
+        case 'L': return 50; break;
+        case 'C': return 100; break;
+        case 'D': return 500; break;
+        case 'M': return 1000; break;
     }
+    return 0;
+}
+int romanToInt(char* s) {
+    int i = 0,result = 0;
+
+    while(s[i] != '\0')
+    {
+        int num = value(s[i]);
+        int num2 = value(s[i+1]);
+
+        if(num < num2 )
+        {
+            result = result - num;
+        } else {
+            result = result + num;
+        }
+        i++;
+    }
+        
     return result;
 }
