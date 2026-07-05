d62ea521091eb2ec28738c7e736d939a3cdd5576
Satyam shaw <mayankrajkkr@gmail.com>
2026-07-06 00:11:25 +0530

    Time: 0 ms (100%), Space: 13.5 MB (18.62%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -7,11 +7,9 @@
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     int carry = 0;
-    
-    struct ListNode dummy; 
-    dummy.next = NULL;
-    
-    struct ListNode *tail = &dummy;
+
+    struct ListNode *head = NULL;
+    struct ListNode *tail = NULL;
     
     while (l1 != NULL || l2 != NULL || carry != 0) {
         int val1 = (l1 != NULL) ? l1->val : 0;
@@ -20,16 +18,24 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
         int sum = val1 + val2 + carry;
         carry = sum / 10;
         
-        struct ListNode *newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->val = sum % 10;
-        newNode->next = NULL;
-        
-        tail->next = newNode;
-        tail = newNode;
+        struct ListNode *newnode = (struct ListNode*)malloc(sizeof(struct ListNode));
+        newnode->val = sum % 10;
+        newnode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        } else 
+        {
+            tail->next = newnode;
+            tail = newnode;
+
+        }
         
         if (l1 != NULL) l1 = l1->next;
         if (l2 != NULL) l2 = l2->next;
     }
     
-    return dummy.next;
+    return head;
 }
