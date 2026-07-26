e9747ed69337a605156267289cebba55cd17987c
Satyam shaw <satyamshaw463@gmail.com>
2026-07-26 23:16:34 +0530

    Time: 0 ms (100%), Space: 48.4 MB (37.99%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,7 +2,6 @@ class Solution {
     public void reverseString(char[] s) {
         int l = 0;
         int r = s.length - 1;
-
         while(l < r)
         {
             char temp = s[r];
@@ -11,6 +10,5 @@ class Solution {
             l++;
             r--;
         }
-        return;
     }
 }
