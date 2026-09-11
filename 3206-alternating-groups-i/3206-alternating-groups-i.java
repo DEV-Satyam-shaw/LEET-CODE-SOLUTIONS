3b606a071a02988941412268ef501284415f96fa
Satyam shaw <satyamshaw463@gmail.com>
2026-09-11 21:36:12 +0530

    Time: 0 ms (100%), Space: 44.4 MB (97.77%) - LeetHub

---
+++ 3206-alternating-groups-i/3206-alternating-groups-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors) {
+        int count = 0;
+        int n = colors.length;
+
+        if(colors[1] != colors[0] && colors[n-1] != colors[0]) count++;
+        if(colors[n-1] != colors[0] && colors[n-2] != colors[n-1]) count++;
+
+        for(int i = 1; i < n-1; i++)
+        {
+            if(colors[i] != colors[i-1] && colors[i] != colors[i+1]) count++;
+        }
+        return count;
+    }
+}
